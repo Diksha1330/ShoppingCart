39ad12f739ef993c1b5e4a78b4c69f33824c4a98
Devangini Dubey <ritudubey95@gmail.com>
2022-03-23 17:04:26 +0530

    Commit 1. MasterDetailScreen 2. App.xaml for colors 3. Style for navigationbar and status bar color 4. resources in Android and iOS

---
+++ ShoppingCart/View/MasterDetailScreenFlyout.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShoppingCart.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterDetailScreenFlyout : ContentPage
+    {
+        public ListView ListView;
+
+        public MasterDetailScreenFlyout()
+        {
+            InitializeComponent();
+
+            BindingContext = new MasterDetailScreenFlyoutViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        private class MasterDetailScreenFlyoutViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<MasterDetailScreenFlyoutMenuItem> MenuItems { get; set; }
+
+            public MasterDetailScreenFlyoutViewModel()
+            {
+                MenuItems = new ObservableCollection<MasterDetailScreenFlyoutMenuItem>(new[]
+                {
+                    new MasterDetailScreenFlyoutMenuItem { Id = 0, Title = "Edit Profile",TitleImage="ic_profile.png" },
+                    new MasterDetailScreenFlyoutMenuItem { Id = 1, Title = "Wishlist" ,TitleImage="ic_wishlist.png"},
+                    new MasterDetailScreenFlyoutMenuItem { Id = 2, Title = "Order History",TitleImage="ic_orderhistory.png" },
+                    new MasterDetailScreenFlyoutMenuItem { Id = 3, Title = "Notifications",TitleImage="ic_notification.png" },
+                   
+                });
+            }
+
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ ShoppingCart/View/MasterDetailScreenFlyoutMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ShoppingCart.View
+{
+    public class MasterDetailScreenFlyoutMenuItem
+    {
+        public MasterDetailScreenFlyoutMenuItem()
+        {
+            TargetType = typeof(MasterDetailScreenFlyoutMenuItem);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ImageSource TitleImage { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
