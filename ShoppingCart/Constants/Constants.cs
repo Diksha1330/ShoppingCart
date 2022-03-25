cd60cd9353955efb20895563f2a1f80455054783
Devangini Dubey <ritudubey95@gmail.com>
2022-03-25 15:46:17 +0530

    Commit Devangini Added constant file Added new Dashboard screen with tabs

---
+++ ShoppingCart/Constants/Constants.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ShoppingCart.Constants
+{
+    public static  class Constants
+    {
+        #region Images
+
+        #region Devangini
+        public static ImageSource HomeIcon = "ic_home.png";
+        public static ImageSource CartIcon = "ic_cart.png";
+        public static ImageSource MyOrderIcon = "ic_myorder.png";
+        public static ImageSource WishlistIcon = "ic_wishlist.png";
+        #endregion
+
+        #region Diksha
+        #endregion
+
+        #endregion
+
+
+        #region Labels
+        #region Devangini
+        public const string HomeTab = "Home";
+        public const string WishListTab = "WishList";
+        public const string MyOrderTab = "My Order";
+        public const string CartTab = "Cart";
+        #endregion
+        #region Diksha
+        #endregion
+        #endregion
+    }
+}
---
+++ ShoppingCart/CustomControl/CustomEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ShoppingCart.CustomControl
+{
+    public class CustomEntry : ContentPage
+    {
+        public CustomEntry()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
